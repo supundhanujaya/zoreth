86a1312553b87113f14991036087a350e7b32089
supundhanujaya <56984494+supundhanujaya@users.noreply.github.com>
2019-10-28 14:26:18 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', ()
+client.on('ready', () =>
 client.user.setGame('Pornhub.com', { type: 'WATCHING' }),
 
 client.on('message', message => {
