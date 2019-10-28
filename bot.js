8ee6fdf19195cb38b98622dfb37326ce36228ac8
supundhanujaya <56984494+supundhanujaya@users.noreply.github.com>
2019-10-28 22:38:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
 
   // If the message content starts with "!kick"
   if (message.content.startsWith('$kick')
-     <message.author.id === '504261424776871936'){
+     <message.author.id === '504261424776871936')) return message.reply("My Boss is **ZORETH!**"){
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
